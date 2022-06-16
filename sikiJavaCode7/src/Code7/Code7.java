8cf88af2446de40b65dbfdf1234f53d0b7a2f842
shyq13 <2700201313@qq.com>
2022-06-16 18:28:57 +0800

    添加项目文件。

---
+++ sikiJavaCode7/src/Code7/Code7.java
@@ -0,0 +1,23 @@
+package Code7;
+
+public class Code7 {
+    public static void main( String[] args ){
+        // 比较运算符
+
+        // 常见比较运算符：
+        // == ：等于
+        // != ：不等于
+        // > ：大于
+        // < ：小于
+        // >= ：大于等于
+        // <= ：小于等于
+        // 注意区分=和==的写法
+
+        // 示例：
+        int a = 10;
+        int b = 20;
+        boolean c = a >= b;
+        boolean d = a <= b;
+        System.out.println("a大于等于b的结果是" + c + "，a小于等于b的结果是" + d);
+    }
+}
