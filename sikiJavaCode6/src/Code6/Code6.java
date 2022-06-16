beaa16125061bd23c86999e7fe65de50d39b348c
shyq13 <2700201313@qq.com>
2022-06-16 18:21:42 +0800

    添加项目文件。

--- sikiJavaCode6/src/Code6/Code6.java
+++ sikiJavaCode6/src/Code6/Code6.java
@@ -14,13 +14,26 @@ public class Code6 {
 
         // 用法：
         // 变量 X= 值;   （X可以是任意的算术运算符）
+        // 上面的算式就相当于：变量 = 变量 X 值;
         // 例：
         int a = 1;
         int b = 2;
         b += a;
         System.out.println("a和b相加的值是" + b);
 
-        // 当然，还可以这样复制（不推荐）：
+        int c = 3;
+        c *= 2;
+        System.out.println("c乘2的值是" + c);
+
+        int d = 4;
+        d /= 2;
+        System.out.println("d除以2的值是" + d);
+
+        int e = 5;
+        e %= 2;
+        System.out.println("e除以2的余数是" + e);
+
+        // 当然，还可以这样赋值（不推荐）：
         int x, y, z;
         x = y = z = 4;
         System.out.println("x,y,z的值分别是" + x + "," + y + "," + z);
