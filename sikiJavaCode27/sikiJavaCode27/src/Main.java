f723d227e9faefce79cf99ad281616ab49278a83
shyq13 <101326128+shyq13@users.noreply.github.com>
2022-12-28 11:50:35 +0800

    Add files via upload
    添加项目文件。

---
+++ sikiJavaCode27/sikiJavaCode27/src/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args) {
+        // 数组
+
+        // 1. 声明数组
+        int array1[];
+        boolean array2[];
+        // I 数据类型 数组名[];
+        char[] array3;
+        String[] array4;
+        // II 数据类型[] 数组名;
+        // 注意：未声明的数组无法使用
+
+        // 2. 初始化
+        int array5[] = new int []{10, 8, 6, 4, 2};
+    }
+}
